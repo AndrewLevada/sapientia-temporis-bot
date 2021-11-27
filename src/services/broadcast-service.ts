939a642357b3886bab93015f012e0c168a6a4608
AndrewLevada <levada.andrew@gmail.com>
2021-11-27 20:50:23 +0300

    Added analytics broadcast

--- src/services/broadcast-service.ts
+++ src/services/broadcast-service.ts
@@ -1,11 +1,18 @@
 import { Telegraf } from "telegraf";
 import { getUsersIdsByGroup } from "./user-service";
 import { groups } from "./groups-service";
+import { logEvent } from "./analytics-service";
 
 export type SpecialBroadcastGroup = "students" | "teachers" | "all" | "5" | "6" | "7" | "8" | "9" | "10" | "11";
 export const specialBroadcastGroupStrings = ["students", "teachers", "all", "5", "6", "7", "8", "9", "10", "11"];
 
 export function broadcastMessage(bot: Telegraf, group: SpecialBroadcastGroup | string, text: string): Promise<number> {
+  logEvent({
+    userId: "admin",
+    name: "broadcast",
+    params: { group, text },
+  });
+
   return getUsersIdsByGroup(specialBroadcastGroupStrings.includes(group) ? group : groups[group]).then(ids => {
     const promises = [];
     let fails = 0;
