209736e7bab39867bf8701c35a88ed22a69c5df1
AndrewLevada <levada.andrew@gmail.com>
2021-12-21 08:20:34 +0300

    Finished csv tools

--- src/tools/csv-tools.ts
+++ src/tools/csv-tools.ts
@@ -5,8 +5,7 @@ import { usersRef } from "./users-query";
 import { UserInfo } from "../services/user-service";
 import { decodeGroupInUserInfo } from "../utils";
 
-// eslint-disable-next-line import/prefer-default-export
-export function makeCsvForInterview(usersIds: string[]): void {
+export function createCSVForInterview(usersIds: string[]): void {
   const users: any[] = [];
   Promise.all(usersIds.map(userId => usersRef.child(userId).once("value")
     .then(snap => (snap.val() ? decodeGroupInUserInfo(snap.val() as UserInfo) : Promise.reject()))
@@ -14,4 +13,12 @@ export function makeCsvForInterview(usersIds: string[]): void {
     .then(user => users.push({ Name: user.name, Group: user.group, Type: user.type, Username: user.username, userId }))
     .catch(() => Promise.resolve())))
     .then(() => fs.writeFileSync("interview.csv", new Parser().parse(users)));
+}
+
+export function createCSVFromUsersDb(pathToJson: string): void {
+  const db = Object.entries(JSON.parse(fs.readFileSync(pathToJson).toString()) as Record<string, UserInfo>);
+  const forCsv = db
+    .map(o => ([o[0], decodeGroupInUserInfo(o[1])]) as [string, UserInfo])
+    .map(o => ({ userId: o[0], crmId: o[0], ...o[1] }));
+  fs.writeFileSync("users_db.csv", new Parser().parse(forCsv));
 }
--- src/tools/launch.ts
+++ src/tools/launch.ts
@@ -1,6 +1,5 @@
 import * as admin from "firebase-admin";
 import { init } from "./users-query";
-import { makeCsvForInterview } from "./csv-tools";
 
 admin.initializeApp({
   credential: admin.credential.cert(JSON.parse(Buffer.from(process.env.FIREBASE_CONFIG as string, "base64").toString("ascii"))),
@@ -9,30 +8,4 @@ admin.initializeApp({
 
 init();
 
-makeCsvForInterview([
-  "879967537",
-  "1091139353",
-  "1101264795",
-  "2045204942",
-  "1263694665",
-  "827359772",
-  "1226773697",
-  "1718977580",
-  "692975559",
-  "1361378845",
-  "1285241455",
-  "1653291225",
-  "1381491635",
-  "1917241757",
-  "1843951102",
-  "1310988554",
-  "685154934",
-  "1787125012",
-  "1242269816",
-  "2013925047",
-  "1876188139",
-  "454928921",
-  "1398985706",
-  "497835989",
-  "2127051412",
-]);
+// Execute tool here
